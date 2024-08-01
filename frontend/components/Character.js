2203458695f13daa09dc9910452d91e7642226a1
Farhana Aktar <farhanaaktar@live.co.uk>
2024-08-01 01:43:58 +0100

    complete

--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -2,9 +2,6 @@ import React, {useState} from 'react'
 
 
 
-
-
-
 function Character({name, homeworld}) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
@@ -18,7 +15,7 @@ function Character({name, homeworld}) { // ❗ Add the props
 
   return (
     <div onClick={toggle} className='character-card' >
-      <h3 className='Character-name'>{name}</h3>
+      <h3 className='character-name'>{name}</h3>
       {renderHomeworld &&
       <p>planet: 
         <span className='character-planet'> {homeworld}</span>
