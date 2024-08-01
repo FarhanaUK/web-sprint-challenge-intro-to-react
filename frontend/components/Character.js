069a3e510e2b3795bdd33daec4967e68ffe2e732
Farhana Aktar <farhanaaktar@live.co.uk>
2024-08-01 01:25:35 +0100

    Styling is working but delayed

--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -4,6 +4,7 @@ import React, {useState} from 'react'
 
 
 
+
 function Character({name, homeworld}) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
