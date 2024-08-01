f02f31e007260683aa09b3d63267d42ffddb580f
Farhana Aktar <farhanaaktar@live.co.uk>
2024-08-01 01:16:14 +0100

    needs styling

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,18 +1,66 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Character from './Character'
 
-const urlPlanets = 'http://localhost:9009/api/planets'
-const urlPeople = 'http://localhost:9009/api/people'
+const urlA = 'http://localhost:9009/api/planets'
+const urlB = 'http://localhost:9009/api/people'
 
 function App() {
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
+  
+  const [people, setPeople] = useState([])
+  const [planets, setPlanets] = useState([])
+
+    useEffect(()=>{
+    function people() {
+     axios.get(urlB)
+    .then(res =>  setPeople(res.data))
+    .catch(err => console.log(err.message))
+    }
+    people()
+
+    },[])
+
+    useEffect(()=>{
+      function planets() {
+       axios.get(urlA)
+      .then(res =>  setPlanets(res.data))
+      .catch(err => console.log(err.message))
+      }
+      planets()
+  
+      },[])
+
+
+
+
+      const profile = people.map((char) => {
+       const home = planets.find((planet) => planet.id === char.homeworld)
+       return {
+        id: char.id,
+        name: char.name,
+        homeworld: {
+          id: char.homeworld,
+          name: home.name
+         
+        }
+      };
+      })
+        console.log(profile)
+        
+
+      
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
+      <h2>
+      { profile.map(ppl => ( <Character key={ppl.id} name={ppl.name} id={ppl.id} homeworld={ppl.homeworld.name}/>))}
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+     
+      </h2>
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Character() { // ❗ Add the props
+
+
+
+
+function Character({name, homeworld}) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const [renderHomeworld, setRenderHomeworld] = useState(false)
+  const toggle = () => {
+    setRenderHomeworld(!renderHomeworld)
+  
+  }
+    
+
+
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    <div onClick={toggle} className='character-card' >
+      <h3 className='Character-name'>{name}</h3>
+      {renderHomeworld &&
+      <p>planet: 
+        <span className='character-planet'> {homeworld}</span>
+      </p>
+}
     </div>
   )
 }
